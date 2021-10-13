8219aba6cc6a9505f9a3165269e59557e6b996e3
Eduard Gavrila <edygavrila@gmail.com>
2021-10-13 23:42:27 +0100

    Written using async/await

--- script.js
+++ script.js
@@ -1,50 +1,37 @@
 const img = document.querySelector("img");
 const form = document.querySelector("form");
 
-const findGyph = (searchString) => {
-  return fetch(
-    `https://api.giphy.com/v1/gifs/translate?s=${searchString}&api_key=kO3uOTLWzAIMjLW1Eq3gZRoIQ89zWY8p`
-  )
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        throw new Error(`Network Error, ${res.status}`);
-      }
-    })
-    .then((data) => {
-      if (data.data.images) {
-        return {
-          url: data.data.images.original.url,
-          status: "OK",
-        };
-      } else {
-        throw new Error("No data for the given search string!");
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      return { error: error };
-    });
+const findGyph = async (searchString) => {
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/translate?s=${searchString}&api_key=kO3uOTLWzAIMjLW1Eq3gZRoIQ89zWY8p`
+    );
+    if (!response.ok) throw new Error("Network Error");
+    const data = await response.json();
+    console.log(data);
+    if (!data.data.images)
+      throw new Error("No results for the given search string!");
+    return {
+      url: data.data.images.original.url,
+    };
+  } catch (error) {
+    console.error(error);
+    return { error: error };
+  }
 };
 
-const getGyphAndSet = (searchStr = "cats") => {
-  findGyph(searchStr)
-    .then((res) => {
-      if (res.error) {
-        img.src = "not-found.jpg";
-        return;
-      }
-      img.src = res.url;
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+const getGyphAndSet = async (searchStr = "cats") => {
+  const result = await findGyph(searchStr);
+  if (result.url) {
+    img.src = result.url;
+  } else {
+    img.src = "not-found.jpg";
+  }
 };
 
 getGyphAndSet("cats");
 
-form.addEventListener("submit", async (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   const searchStr = document.getElementById("search").value.trim();
   getGyphAndSet(searchStr);
